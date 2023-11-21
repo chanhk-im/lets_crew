546ca3bc19a9e5cc925749754c9aa9307c1b1512
chanhk-im <cfasd1875@gmail.com>
2023-11-21 16:32:03 +0900

    add screen util

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'login.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shrine',
-      initialRoute: '/login',
-      routes: {
-        '/login': (BuildContext context) => LoginPage(),
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Shrine',
+          initialRoute: '/login',
+          routes: {
+            '/login': (BuildContext context) => LoginPage(),
+          },
+        );
       },
     );
   }
