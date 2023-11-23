1b8230564fbc5e60e46c78ac590b820910b2fb38
eu2goo <eu2goo@gmail.com>
2023-11-24 02:38:41 +0900

    feat: 익명 로그인 생성

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lets_crew/home.dart';
 
 import 'login.dart';
 
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
           initialRoute: '/login',
           routes: {
             '/login': (BuildContext context) => LoginPage(),
+            '/': (BuildContext context) => HomePage(),
           },
         );
       },
---
+++ lib/app_state.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AppState {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final CollectionReference _usersCollection =
+      FirebaseFirestore.instance.collection('user');
+
+  Future<User?> handleGoogleSignIn() async {
+    print("1");
+
+    try {
+      // Google Sign-In
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Firebase Authentication
+      final UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      final User? user = userCredential.user;
+
+      // Check if the user already exists in Firestore
+      final DocumentSnapshot userSnapshot =
+          await _usersCollection.doc(user?.uid).get();
+
+      if (!userSnapshot.exists) {
+        // User does not exist in Firestore, add them
+        await _usersCollection.doc(user?.uid).set({
+          'uid': user?.uid,
+          'name': user?.displayName,
+          'email': user?.email,
+          'role': false,
+        });
+      }
+
+      return user;
+    } catch (e) {
+      print('Google Sign-In Error: $e');
+      return null;
+    }
+  }
+
+  Future<User?> anonymousLogin() async {
+    try {
+      final userCredential = await FirebaseAuth.instance.signInAnonymously();
+      print("Signed in with temporary account.");
+      final User? user = userCredential.user;
+      final DocumentSnapshot userSnapshot =
+          await _usersCollection.doc(user?.uid).get();
+      if (!userSnapshot.exists) {
+        // User does not exist in Firestore, add them
+        await _usersCollection.doc(user?.uid).set({
+          'uid': user?.uid,
+          'role': false,
+        });
+      }
+      return user;
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "operation-not-allowed":
+          print("Anonymous auth hasn't been enabled for this project.");
+          break;
+        default:
+          print("Unknown error.");
+      }
+      return null;
+    }
+  }
+}
